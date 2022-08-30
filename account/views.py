63249bcb2c8c7c3c1467c7a722ff846c9bc8688d
Sun Ho <tjsgh5312@daum.net>
2022-08-30 17:01:26 +0900

    16강

--- account/views.py
+++ account/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 # Create your views here.
 def hello_world(request):
-    return  render(request, 'account/helloworld.html')
+    if request.method =="POST":
+        return render(request, 'account/helloworld.html', context ={'text': 'POST METHOD!!!'})
+    else:        
+        return render(request, 'account/helloworld.html', context ={'text': 'GET METHOD!!!'})
