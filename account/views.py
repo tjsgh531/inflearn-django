c033dd8c7af03ea3a6db8a02ad43cc56c453d1df
Sun Ho <tjsgh5312@daum.net>
2022-08-30 15:50:42 +0900

    9강

--- account/views.py
+++ account/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def hello_world(request):
-    return  render(request, 'base.html')
+    return  render(request, 'account/helloworld.html')
--- config/settings.py
+++ config/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
-
 import environ
 import os
+
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,20 +22,17 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(
+    env_file = os.path.join(BASE_DIR, '.env')
+)
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
+DEBUG = env('DEBUG')
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+
 
 ALLOWED_HOSTS = []
 
