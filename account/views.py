c002ea43d12be55597580a8c51fd3d2fc80ec9df
Sun Ho <tjsgh5312@daum.net>
2022-08-30 17:08:51 +0900

    17강

--- account/views.py
+++ account/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-# Create your views here.
+from .models import HelloWorld
+
 def hello_world(request):
     if request.method =="POST":
-        return render(request, 'account/helloworld.html', context ={'text': 'POST METHOD!!!'})
+
+        temp = request.POST.get('hello_world_input')
+        
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'account/helloworld.html', context ={'hello_world_output': new_hello_world})
     else:        
         return render(request, 'account/helloworld.html', context ={'text': 'GET METHOD!!!'})
